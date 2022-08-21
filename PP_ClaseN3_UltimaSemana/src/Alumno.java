1c83ade6647cae038a2ff113d06661c3cfe2d3e3
OctavioCortez24 <oc31549@gmail.com>
2022-08-21 16:25:55 -0300

    Creo los archivos

--- PP_ClaseN3_UltimaSemana/src/Alumno.java
+++ PP_ClaseN3_UltimaSemana/src/Alumno.java
@@ -44,3 +44,4 @@ public class Alumno {
         this.edad = edad;
     }
 }
+
--- PP_ClaseN3_UltimaSemana/src/Main.java
+++ PP_ClaseN3_UltimaSemana/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        Alumno alumno=new Alumno("Leandro","Cortez","45876507","17");
     }
 }
