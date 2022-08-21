53ee6a1bf526895f76f1e8380bb53cdcd424848b
OctavioCortez24 <oc31549@gmail.com>
2022-08-21 16:20:35 -0300

    Subo la libreria externa

--- PP_ClaseN3_UltimaSemana/Alumno.java
+++ PP_ClaseN3_UltimaSemana/Alumno.java
@@ -1,2 +1,46 @@
 public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String edad;
+    public Alumno(){
+
+    }
+    public Alumno(String nombre,String apellido,String dni,String edad){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.dni=dni;
+        this.edad=edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 }
